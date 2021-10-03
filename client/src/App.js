9281ba9643c98d4a422b7969189a35d57f01f8f2
Brylynn Cox <carl.cc@gmail.com>
2021-10-03 07:54:29 -0500

    deployment test 2

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Don't Learn React
+            Fib Calculator
           </a><br />
           <Fib />
         </header>
